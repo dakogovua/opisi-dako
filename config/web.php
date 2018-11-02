c34276b1a514b261ff3f671b7c1ac45646ed24b7
admin <ingener@dako.gov.ua>
2018-11-02 12:45:46 +0200

    htaccess and config base url

--- config/web.php
+++ config/web.php
@@ -17,6 +17,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'dakoshmakosecretkey',
+            'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
