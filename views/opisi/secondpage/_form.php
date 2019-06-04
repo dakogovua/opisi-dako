42eb05827f2fa622cbdc900e0c23d5785febdc13
admin <ingener@dako.gov.ua>
2019-06-05 00:02:16 +0300

    added drop down list

--- views/opisi/secondpage/_form.php
+++ views/opisi/secondpage/_form.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use app\models\opisi\Firstpage;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\opisi\Secondpage */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,7 +14,12 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'papka')->textarea(['rows' => 6]) ?>
+    <?php // echo $form->field($model, 'papka')->textarea(['rows' => 6]); ?>
+
+    <?php
+    $modelka = Firstpage::useTable($_GET['sectablename']);
+        echo $form->field($model, 'papka')
+		->dropDownList(\yii\helpers\ArrayHelper::map($modelka::find()->all(), 'id', 'papka')); ?>
 
     <?= $form->field($model, 'podpapka')->textInput(['maxlength' => true]) ?>
 
