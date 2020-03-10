fe9553b6e110fd087e5f6b4277486a027af50e2c
admin <ingener@dako.gov.ua>
2020-03-10 16:58:42 +0200

    test hostname revert

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $hostname = $_SERVER['SERVER_NAME'];
-echo $hostname;
+// echo $hostname;
 
 // comment out the following two lines when deployed to production
  defined('YII_DEBUG') or define('YII_DEBUG', true);
