068c39ad617cc1600ab3d1c608d64ba2afadb7ac
admin <ingener@dako.gov.ua>
2020-03-10 17:07:57 +0200

    1

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 $hostname = $_SERVER['SERVER_NAME'];
 
-echo $hostname;
+//echo $hostname;
 
 // comment out the following two lines when deployed to production
  defined('YII_DEBUG') or define('YII_DEBUG', true);
